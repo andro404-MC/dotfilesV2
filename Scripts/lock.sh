443772bdd394608acd543dd7e7403dc4871a094f
untemi <94703538+andro404-MC@users.noreply.github.com>
2024-08-11 21:45:05 +0100

    Improoooving that hypr

---
+++ Scripts/lock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+swayidle -w timeout 10 'hyprctl dispatch dpms off' \
+            resume 'hyprctl dispatch dpms on' &
+SWAYIDLE_PID=$!
+
+gtklock --idle-hide --idle-timeout 10
+
+kill $SWAYIDLE_PID
+exit
