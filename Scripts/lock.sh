c26df3ca40342d79d41352192edabde0baed0c23
untemi <94703538+andro404-MC@users.noreply.github.com>
2024-08-15 00:11:49 +0100

    finnaly conly on wayland

--- Scripts/lock.sh
+++ Scripts/lock.sh
@@ -4,7 +4,8 @@ swayidle -w timeout 10 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' &
 SWAYIDLE_PID=$!
 
-gtklock --idle-hide --idle-timeout 10
+# gtklock --idle-hide --idle-timeout 10
+hyprlock
 
 kill $SWAYIDLE_PID
 exit
