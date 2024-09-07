e8f874226c429712fb1538dbf55c488c98b6ae5a
Untemi <94703538+andro404-MC@users.noreply.github.com>
2024-09-07 13:21:11 +0100

    Create config-init.sh

---
+++ Scripts/config-init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+git clone --bare https://github.com/andro404-MC/dotfilesV2 $HOME/.dotfiles
+# define config alias locally since the dotfiles
+# aren't installed on the system yet
+function config {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+# create a directory to backup existing dotfiles to
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles from https://github.com/andro404-MC/dotfilesV2";
+  else
+    echo "Moving existing dotfiles to ~/.dotfiles-backup";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+# checkout dotfiles from repo
+config checkout
+config config status.showUntrackedFiles no
