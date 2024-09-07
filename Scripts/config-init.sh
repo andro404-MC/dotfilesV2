dcdc660aa1ef272f6ab3fa9802dcf2b3e9e25ad5
Untemi <94703538+andro404-MC@users.noreply.github.com>
2024-09-07 15:04:47 +0100

    init

--- Scripts/config-init.sh
+++ Scripts/config-init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-git clone --bare https://github.com/andro404-MC/dotfilesV2 $HOME/.dotfiles
+git clone --bare https://github.com/andro404-MC/dotfilesV2 $HOME/.local/share/dotfiles
 # define config alias locally since the dotfiles
 # aren't installed on the system yet
 function config {
-   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+   git --git-dir=$HOME/.local/share/dotfiles --work-tree=$HOME $@
 }
 # create a directory to backup existing dotfiles to
 mkdir -p .dotfiles-backup
@@ -17,4 +17,15 @@ fi
 # checkout dotfiles from repo
 config checkout
 config config status.showUntrackedFiles no
+
+echo '
+[rumpowered]
+Server = https://jc141x.github.io/rumpowered-packages/$arch ' | sudo tee -a /etc/pacman.conf
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo pacman-key --recv-keys cc7a2968b28a04b3
+sudo pacman-key --lsign-key cc7a2968b28a04b3
+sudo pacman -Syyu
+sudo pacman -S yay
+
+sudo yay --needed -S yazi zsh zoxide starship cfonts neovim eza ripgrep fzf go trash-cli curl fastfetch
 ya pack -i
