434ecf0d2a570909992a4855645cf0cf83e05bb1
untemi <94703538+andro404-MC@users.noreply.github.com>
2024-09-08 16:46:54 +0100

    init++

--- Scripts/config-init.sh
+++ Scripts/config-init.sh
@@ -18,6 +18,8 @@ config checkout
 config config status.showUntrackedFiles no
 
 # YAY
+
+echo "Adding rumpowered"
 echo '
 [rumpowered]
 Server = https://jc141x.github.io/rumpowered-packages/$arch ' | sudo tee -a /etc/pacman.conf
@@ -25,21 +27,38 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo pacman-key --recv-keys cc7a2968b28a04b3
 sudo pacman-key --lsign-key cc7a2968b28a04b3
 sudo pacman -Syyu
+
+echo "Installing YAY"
 sudo pacman -S yay
 
 # PACKAGES
-yay --needed -S albert autotiling base base-devel bemenu blueberry btop cfonts cliphist dunst engrampa eza fastfetch firefox foot fzf git go gtklock htop hyprlock imv intel-gpu-tools intel-media-driver keepassxc kvantum kvantum-qt5 less libva-intel-driver libva-utils mpc mpd mpv ncmpcpp nemo neovim network-manager-applet networkmanager nm-connection-editor nodejs noto-fonts-cjk npm nsxiv nwg-look p7zip polkit-gnome qbittorrent qt5ct qt6ct ripgrep rofi-calc-git rofi-wayland starship sway swaybg swayidle swayosd-git thunderbird trash-cli ttf-font-awesome ttf-jetbrains-mono-nerd ttf-ubuntu-font-family udiskie unzip waybar wl-clip-persist wl-clipboard wlogout xdg-desktop-portal-wlr xdg-user-dirs xorg-xwayland yazi yt-dlp zathura zathura-cb zathura-ps zoxide zsh dash glow perl-image-exiftool pavucontrol ttf-apple-emoji xorg-xrdb tumbler ffmpegthumbnailer
+echo "Installing Software"
+yay --needed -S albert autotiling base base-devel bemenu blueberry btop cfonts cliphist dunst engrampa eza fastfetch firefox foot fzf git go gtklock htop hyprlock imv intel-gpu-tools intel-media-driver keepassxc kvantum kvantum-qt5 less libva-intel-driver libva-utils mpc mpd mpv ncmpcpp nemo neovim network-manager-applet networkmanager nm-connection-editor nodejs noto-fonts noto-fonts-cjk ttf-dejavu npm nsxiv nwg-look p7zip polkit-gnome qbittorrent qt5ct qt6ct ripgrep rofi-calc-git rofi-wayland starship sway swaybg swayidle swayosd-git thunderbird trash-cli ttf-font-awesome ttf-jetbrains-mono-nerd ttf-ubuntu-font-family udiskie unzip waybar wl-clip-persist wl-clipboard wlogout xdg-desktop-portal-wlr xdg-desktop-portal-gtk xdg-user-dirs xorg-xwayland yazi yt-dlp zathura zathura-cb zathura-ps zoxide zsh dash glow perl-image-exiftool pavucontrol ttf-apple-emoji xorg-xrdb tumbler ffmpegthumbnailer gamemode qt5-wayland flatpak nemo-engrampa wget
 
+echo "Setting up Yazi"
 ya pack -i
+
+echo "Setting up swayosd"
 sudo systemctl enable --now swayosd-libinput-backend.service
 sudo usermod -a -G video $USER
 
+echo "Setting up zsh history"
 mkdir .cache/zsh/
 touch .cache/zsh/history
+
+echo "Setting up mpd"
 mkdir .local/share/mpd
 mkdir .local/share/mpd/playlists
+
+echo "Setting dash as default system shell"
 sudo rm /usr/bin/sh
 sudo ln /usr/bin/dash /usr/bin/sh
 
+echo "Setting foot as xterm"
+sudo ln /usr/bin/foot /usr/bin/xterm
+
+echo "Font cache..."
 fc-cache -fr
+
+echo "user-dirs-update..."
 xdg-user-dirs-update
